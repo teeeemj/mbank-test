e2ec27dff4076bf7619f6a3c4c990d2f1269a8cd
teeeemj <teeeemj@gmail.com>
2025-05-18 20:29:32 +0600

    refactored data layer

--- lib/data/repositories/event_repository_impl.dart
+++ lib/data/repositories/event_repository_impl.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:mbank_test_calendar/core/error/error_handler.dart';
 import 'package:mbank_test_calendar/data/datasources/remote/event_remote_data_source.dart';
+import 'package:mbank_test_calendar/data/models/event_resp_model.dart';
 import 'package:mbank_test_calendar/domain/entities/event.dart';
 import 'package:mbank_test_calendar/core/error/failures.dart';
 import 'package:mbank_test_calendar/domain/repositories/event_repository.dart';
@@ -24,10 +25,14 @@ class EventRepositoryImpl implements EventRepository {
         startDate: startDate,
         endDate: endDate,
       );
-      final events = eventModels.map((model) => model.toEntity()).toList();
+      final events = _mapToEntities(eventModels);
       return Right(events);
     } catch (e) {
       return Left(errorHandler.handleError(e));
     }
+  }
+
+  List<Event> _mapToEntities(List<EventRespModel> models) {
+    return models.map((model) => model.toEntity()).toList();
   }
 }
